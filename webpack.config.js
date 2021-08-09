89e08043ae8f3d2bb32e8e2431d2d71e268300da
Nicholas Fazzolari <nickfazzpdx@gmail.com>
2021-08-08 19:29:57 -0700

    Added webpack watch npm script.

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ var path = require('path');
 
 module.exports = {
     context: path.resolve(__dirname, './src/ts'),
+    watch: true,
     mode: 'development',
     entry: {
         main: './main.ts'
